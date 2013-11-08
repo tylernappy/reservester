b2c99e7ffc937e3e2350b8042e18225f02e2809e
Tyler Nappy <tylernappy@gmail.com>
2013-11-08 18:57:49 -0500

    first commit for lab 2

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,58 @@
 class RestaurantsController < ApplicationController
-	def new
+  def index
+  	@restaurants = Restaurant.all
+  end
 
-	end
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def new
+  	@restaurant = Restaurant.new
+  end
+
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+
+ 
+  def create
+  	@restaurant = Restaurant.new(params[:restaurant].permit(:name, :description, :address, :phone))
+
+    if @restaurant.save
+      redirect_to @restaurant, notice: 'Restaurant was successfully created.'
+    else
+      render action: "new"
+    end
+  	# @restaurant = Restaurant.new(restaurant_params)
+
+  	# @restaurant.save
+  	# redirect_to @restaurant
+
+  	# render text: params[:restaurant].inspect
+  end	
+
+  def update
+    @restaurant = Restaurant.find(params[:id])
+
+    if @restaurant.update_attributes(params[:restaurant].permit(:name, :description, :address, :phone))
+      redirect_to @restaurant, notice: 'Restaurant was successfully updated.'
+    else
+      render action: "edit"
+    end
+  end
+
+  def destroy
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.destroy
+
+    redirect_to restaurants_url
+  end
+
+  private
+  def post_params
+  	params.require(:restaurant).permit(:name, :description, :address, :phone)
+  end
+
 end
