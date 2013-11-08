1a1f0135beee01b85c13ea098229ddf11bd963af
Tyler Nappy <tylernappy@gmail.com>
2013-11-08 16:58:37 -0500

    first commit for beggining of lab 2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Reservester::Application.config.secret_key_base = '4217dc5c1c13cd619c6ca51fea4fc0ee626dedc968359edf0d1ac5470cb6af70a6c0597c42a2833a774bde1749d142a05ed2c384d55868c3e6c49eefde775db0'
